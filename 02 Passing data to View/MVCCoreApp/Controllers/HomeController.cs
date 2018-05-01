95de4e3c4e973f97fc71287df2980082267bfcb5
Tektutorialshub <admin@tektutorialshub.com>
2018-05-01 10:11:17 +0530

    Initial commit

---
+++ 02 Passing data to View/MVCCoreApp/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+using MVCCoreApp.Models;
+
+namespace MVCCoreApp.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            ViewBag.Customer = new Customer();
+            return View();
+        }
+
+        public IActionResult Method1()
+        {
+            ViewData.Model = new Customer(); 
+            return View();
+        }
+
+
+        public IActionResult StrongView()
+        {
+            ViewData.Model = new Customer();
+            return View();
+        }
+
+
+        public IActionResult CorrectWay()
+        {
+            Customer customer = new Customer();
+            return View(customer);
+        }
+
+    }
+}
